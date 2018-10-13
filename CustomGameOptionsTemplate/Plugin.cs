73d79d1650ee38deba33ae2f0fb744a3aeb6a8a0
Moon <q@q>
2018-10-13 00:04:57 -0500

    First commit

---
+++ CustomGameOptionsTemplate/Plugin.cs
@@ -0,0 +1,116 @@
+﻿using IllusionPlugin;
+using UnityEngine.SceneManagement;
+
+namespace CustomGameOptionsTemplate
+{
+    public class Plugin : IPlugin
+    {
+        public string Name => "CustomGameOptionsTemplate";
+        public string Version => "0.0.1";
+        public void OnApplicationStart()
+        {
+            SceneManager.activeSceneChanged += SceneManagerOnActiveSceneChanged;
+            SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+        }
+
+        private void SceneManagerOnActiveSceneChanged(Scene arg0, Scene arg1)
+        {
+            if (arg1.name == "Menu")
+            {
+                var ctOption = GameOptionsUI.CreateListOption("ChromaList1");
+                ctOption.AddOption(1, "ONE");
+                ctOption.AddOption(2, "TWO");
+                ctOption.AddOption(3, "THREE");
+                ctOption.AddOption(4, "FOUR");
+                ctOption.GetValue = () => 2f;
+                ctOption.OnChange += (e) => Logger.Info($"ONCHANGE1: {e}");
+
+                ctOption = GameOptionsUI.CreateListOption("ChromaList2");
+                ctOption.AddOption(1, "ONE");
+                ctOption.AddOption(2, "TWO");
+                ctOption.AddOption(3, "THREE");
+                ctOption.AddOption(4, "FOUR");
+                ctOption.GetValue = () => 1f;
+                ctOption.OnChange += (e) => Logger.Info($"ONCHANGE2: {e}");
+
+                ctOption = GameOptionsUI.CreateListOption("ChromaList3");
+                ctOption.AddOption(1, "ONE");
+                ctOption.AddOption(2, "TWO");
+                ctOption.AddOption(3, "THREE");
+                ctOption.AddOption(4, "FOUR");
+                ctOption.GetValue = () => 1f;
+                ctOption.OnChange += (e) => Logger.Info($"ONCHANGE3: {e}");
+
+                ctOption = GameOptionsUI.CreateListOption("ChromaList4");
+                ctOption.AddOption(1, "ONE");
+                ctOption.AddOption(2, "TWO");
+                ctOption.AddOption(3, "THREE");
+                ctOption.AddOption(4, "FOUR");
+                ctOption.GetValue = () => 2f;
+                ctOption.OnChange += (e) => Logger.Info($"ONCHANGE4: {e}");
+
+                var ctToggle = GameOptionsUI.CreateToggleOption("ChromaToggle1");
+                ctToggle.Default = true;
+                ctToggle.OnToggle += (b) => Logger.Info($"TOGGLE1: {b}");
+
+                ctToggle = GameOptionsUI.CreateToggleOption("ChromaToggle2");
+                ctToggle.OnToggle += (b) => Logger.Info($"TOGGLE2: {b}");
+
+                ctToggle = GameOptionsUI.CreateToggleOption("ChromaToggle3");
+                ctToggle.OnToggle += (b) => Logger.Info($"TOGGLE3: {b}");
+
+                ctToggle = GameOptionsUI.CreateToggleOption("ChromaToggle4");
+                ctToggle.Default = true;
+                ctToggle.OnToggle += (b) => Logger.Info($"TOGGLE4: {b}");
+
+                ctOption = GameOptionsUI.CreateListOption("ChromaList5");
+                ctOption.AddOption(1, "ONE");
+                ctOption.AddOption(2, "TWO");
+                ctOption.AddOption(3, "THREE");
+                ctOption.AddOption(4, "FOUR");
+                ctOption.GetValue = () => 1f;
+                ctOption.OnChange += (e) => Logger.Info($"ONCHANGE5: {e}");
+
+                ctOption = GameOptionsUI.CreateListOption("ChromaList6");
+                ctOption.AddOption(1, "ONE");
+                ctOption.AddOption(2, "TWO");
+                ctOption.AddOption(3, "THREE");
+                ctOption.AddOption(4, "FOUR");
+                ctOption.GetValue = () => 2f;
+                ctOption.OnChange += (e) => Logger.Info($"ONCHANGE6: {e}");
+
+                ctToggle = GameOptionsUI.CreateToggleOption("ChromaToggle5");
+                ctToggle.OnToggle += (b) => Logger.Info($"TOGGLE5: {b}");
+
+                GameOptionsUI.Build();
+            }
+        }
+
+        private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
+        {
+        }
+
+        public void OnApplicationQuit()
+        {
+            SceneManager.activeSceneChanged -= SceneManagerOnActiveSceneChanged;
+            SceneManager.sceneLoaded -= SceneManager_sceneLoaded;
+        }
+
+        public void OnLevelWasLoaded(int level)
+        {
+
+        }
+
+        public void OnLevelWasInitialized(int level)
+        {
+        }
+
+        public void OnUpdate()
+        {
+        }
+
+        public void OnFixedUpdate()
+        {
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+CustomGameOptionsTemplate
+======
+
+THIS IS A PERSONAL PROJECT. ANY CODE WITHIN IS NOT INHERENTLY MY OWN, BUT PROPERTY OF ITS RESPECTIVE AUTHOR.
+
+Important Info
+======
+
+As of right now, 10/13/2018, this code is designated for use in ChromaToggle.
+I say this because as of right now, if two developers were to try to use this helper class,
+UI chaos would ensue. This is due to the fact that each particular instance of the helper would try to
+make modifications to the object hierarchy without knowledge of each other.
+
+One day, maybe, this will be fixed, but now that I've gotten this far I'm gonna take a break and
+focus on my ScoreSaber rank ;)
